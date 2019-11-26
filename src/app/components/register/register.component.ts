689f8d6f4e7685618c949497f5034b920233198c
Roberto Kenzo Hamano <roberto@bluetechno.com.br>
2019-11-25 22:26:37 -0300

    Improving css

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -6,10 +7,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
+  recordDay: Date = new Date();
+  answer: string[] = [
+    "1",
+    "nada",
+    "comi pizza",
+    "sem gasosa",
+    "a mim mesmo"
+    ];
 
-  constructor() { }
+  constructor(
+    private router: Router
+  ) { }
 
   ngOnInit() {
   }
 
+  goBack = () => this.router.navigate([`listRegistry`]);
 }
