618be85aee8c9b290e37c8ebcc661c7a93ed7a15
Halk Liff <halkliff@pm.me>
2019-12-02 11:13:23 -0300

    Improvement over a couple things, and search by date.

--- src/app/services/record.service.ts
+++ src/app/services/record.service.ts
@@ -1,9 +1,9 @@
 import { Injectable } from '@angular/core';
-import {AngularFirestore, AngularFirestoreCollection, DocumentReference} from "@angular/fire/firestore";
-import {Observable} from "rxjs";
-import RecordInterface from "../interfaces/record.interface";
+import {AngularFirestore, AngularFirestoreCollection, DocumentReference} from '@angular/fire/firestore';
+import {Observable} from 'rxjs';
+import RecordInterface from '../interfaces/record.interface';
 import uuid from 'uuid/v4';
-import {UserService} from "./user.service";
+import {UserService} from './user.service';
 
 @Injectable({
   providedIn: 'root'
@@ -49,7 +49,7 @@ export class RecordService {
   public async getSingleRecord(id: string): Promise<RecordInterface | null> {
     const col = this.db.collection<RecordInterface>(
       RecordService.DB_COLLECTION_KEY,
-        ref => ref.where('id', '==',id)
+        ref => ref.where('id', '==', id)
     );
     const items = (await col.get().toPromise());
     if (items.empty) {
@@ -58,7 +58,7 @@ export class RecordService {
 
     // The IDs are Unique UUIDs, so this query should return only one document.
     // With that in mind, return just the document at the first index of the array.
-    return items[0];
+    return items.docs[0].data() as RecordInterface;
   }
 
   /**
@@ -76,5 +76,36 @@ export class RecordService {
     // With that in mind, delete just the document at the first index of the array.
     await items.docs[0].ref.delete();
     return true;
+  }
+
+  /**
+   * Searches a document or range of documents by date.
+   * @param date the date parameter.
+   * @returns an array containing the documents found by the provided date.
+   *
+   * Usage:
+   * ```
+   * const date = new Date(2019, 11, 22);
+   * const data = await service.searchByDate(date); // Array<RecordInterface>
+   * ```
+   */
+  public async searchByDate(date: Date): Promise<RecordInterface[]> {
+    const dateLess = new Date(date);
+    dateLess.setDate(date.getDate() - 1);
+    const col = this.db.collection<RecordInterface>(
+      RecordService.DB_COLLECTION_KEY,
+      ref => ref
+        .where('createdAt', '>', dateLess)
+        .where('createdAt', '<=', date)
+    );
+    const items = (await col.get().toPromise());
+    if (items.empty) {
+      return [];
+    }
+    const returnItems: RecordInterface[] = [];
+
+    items.docs.forEach(doc => returnItems.push(doc.data() as RecordInterface));
+
+    return returnItems;
   }
 }
